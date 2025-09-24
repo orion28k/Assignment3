5890b332c80c545c6b6aee69d6b51209b0092ad8
orion28k <160691449+orion28k@users.noreply.github.com>
2025-09-24 17:00:58 -0400

    chore: add DB ping

--- ClassLibrary1/MongoDBConnector.cs
+++ ClassLibrary1/MongoDBConnector.cs
@@ -1,8 +1,17 @@
-﻿namespace ClassLibrary1;
+﻿using System.Xml.XPath;
+
+namespace ClassLibrary1;
+
 public class MongoDBConnector
 {
     public MongoDBConnector(string name)
     {
-        
+
+    }
+
+    public bool PingMongoDB()
+    {
+        bool success = true;  
+        return success;
     }
 }
